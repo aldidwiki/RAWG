2f7279885f966fa83c8abb8ae1f03f7951d4cd69
Aldi Prahasta <aldidwiki@gmail.com>
2022-10-16 20:36:48 +0700

    add plist file

--- RAWG/NetworkSource/NetworkService.swift
+++ RAWG/NetworkSource/NetworkService.swift
@@ -8,9 +8,22 @@
 import Foundation
 
 class NetworkService {
-    let apiKey = "bc5e5774b10d40f7b8768e231b155c65"
     let page = "1"
     let pageSize = "15"
+    private var apiKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "RAWG-Info", ofType: "plist") else {
+                fatalError("Couldn't find file 'RAWG-Info.plist'.")
+            }
+            
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "API_KEY") as? String else {
+                fatalError("Couldn't find key 'API_KEY' in 'RAWG-Info.plist'.")
+            }
+            
+            return value
+        }
+    }
     
     func getGames(searchQuery: String? = nil) async throws -> [Game] {
         var components = URLComponents(string: "https://api.rawg.io/api/games")!
